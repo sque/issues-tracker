7a20f11ee2d8f3cdc9e337ae32d6096038be9993
sque <squarious@gmail.com>
2010-06-08 06:53:14 +0300

    Improve attachments form to selectively show one more field.

--- static/js/issues.js
+++ static/js/issues.js
@@ -11,10 +11,24 @@ $(document).ready(function() {
             $(this).animate({ 'background-color': start_color}, 400, function(){ $(this).css({ 'background-color': null});});
     });*/
     
+    // More button on forms
     $('.ui-form-more').click(function(){
         $(this).parents('dt').nextAll('dt').toggle('fast');
         $(this).toggleClass('pressed');
     })
     .parents('dt').nextAll('dt').hide();
+
+
+    // Hide attachments
+    $('.ui-form-attachments-start').parents('dt').append(
+        $('<a class="ui-form-more-attachments">Upload another file ...</a>').click(function(){
+            var cur_name = $(this).prev().attr('name');
+            var next = $(this).parents('div')
+                .find('input[name=attachment' + (parseInt(cur_name.replace(/attachment/, ' ')) + 1) + ']');
+            $(this).detach().appendTo(next.parent());
+            next.show(80);
+        })
+    ).nextAll().find('input[type=file]').hide();
+    
 
 });
